e5cfd8cc1b4fc8c0eaa9ba348b2ba2e778c63dc7
ZA Rakib <phpzarakib@gmail.com>
2021-04-02 23:12:06 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
   const [loggedInUser, setLoggedInUser]= useState({})
   return (
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-      <p>Name : {loggedInUser.name}</p>
       <Router>
         <Header />
         <Switch>
